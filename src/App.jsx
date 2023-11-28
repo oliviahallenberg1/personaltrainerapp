93d4e9ecaca0b85f52f1b47100b51b9cc6938b48
oliviahallenberg1 <bgz829@myy.haaga-helia.fi>
2023-11-28 15:47:10 +0200

    updated tabs to router

--- src/App.jsx
+++ src/App.jsx
@@ -7,51 +7,69 @@ import { useState } from 'react';
 import { Tab, Tabs } from '@mui/material';
 import './App.css';
 
+import { Link, Outlet } from 'react-router-dom';
+
 function App() {
 
-  const [value, setValue] = useState('Home');
-
-  const handleChange = (_, value) => {
-    setValue(value);
-  }
-
   return (
-    <>
-      <div>
-        <Tabs
-          TabIndicatorProps={{ style: { background: 'black' }, text: 'black' }}
-          value={value}
-          onChange={handleChange}
-          centered={true}
-          color="pink"
-          style={{ backgroundColor: 'pink' }}
-          textColor="inherit"
-          indicatorColor="black">
-          <Tab
-            value="Home"
-            label="Home">
-          </Tab>
-          <Tab
-            value="Customer"
-            label="Customer">
-          </Tab>
-          <Tab
-            value="Training"
-            label="Training">
-          </Tab>
-          <Tab
-            value="TrainingCalendar"
-            label="Calendar">
-          </Tab>
-        </Tabs>
-        {value === "Home" && <Home />}
-        {value === "Customer" && <Customer />}
-        {value === "Training" && <Training />}
-        {value === "TrainingCalendar" && <TrainingCalendar />}
 
-      </div>
+    <>
+
+      <nav>
+        <Link to={'/'}>Home </Link>
+        <Link to={'/customers'}>Customers </Link>
+        <Link to={'/trainings'}>Trainings </Link>
+        <Link to={'/calendar'}>Calendar </Link>
+      </nav>
+      <Outlet />
+
     </>
-  )
+
+  );
+
+  // const [value, setValue] = useState('Home');
+
+  // const handleChange = (_, value) => {
+  //   setValue(value);
+  // }
+
+  // return (
+  //   <>
+  //     <div>
+  //       <Tabs
+  //         TabIndicatorProps={{ style: { background: 'black' }, text: 'black' }}
+  //         value={value}
+  //         onChange={handleChange}
+  //         centered={true}
+  //         color="pink"
+  //         style={{ backgroundColor: 'pink' }}
+  //         textColor="inherit"
+  //         indicatorColor="black">
+  //         <Tab
+  //           value="Home"
+  //           label="Home">
+  //         </Tab>
+  //         <Tab
+  //           value="Customer"
+  //           label="Customer">
+  //         </Tab>
+  //         <Tab
+  //           value="Training"
+  //           label="Training">
+  //         </Tab>
+  //         <Tab
+  //           value="TrainingCalendar"
+  //           label="Calendar">
+  //         </Tab>
+  //       </Tabs>
+  //       {value === "Home" && <Home />}
+  //       {value === "Customer" && <Customer />}
+  //       {value === "Training" && <Training />}
+  //       {value === "TrainingCalendar" && <TrainingCalendar />}
+
+  //     </div>
+  //   </>
+  // )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+//import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import TrainingCalendar from './components/TrainingCalendar.jsx'
+import TrainingGrid from './components/TrainingGrid.jsx'
+import CustomerGrid from './components/CustomerGrid.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+  { path: "/",
+  element: <App/>,
+  children: [
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/customers",
+    element: <CustomerGrid/>
+  },
+  {
+    path: "/trainings",
+    element: <TrainingGrid/>
+  },
+  {
+    path: "/calendar",
+    element: <TrainingCalendar/>
+  }
+  ]
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
